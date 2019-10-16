f4a36af745b8d72d55a9519aefcd629c25f86806
Ryan Luu <ryan@vumble.com>
2019-10-15 22:56:39 -0400

    Updates

---
+++ precache-manifest.a10b29372585f0630c77d3321c42582f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2038d3d92bac7418786d2822f09996",
+    "url": "/vumble2/index.html"
+  },
+  {
+    "revision": "eb9fe4f79f693461cdba",
+    "url": "/vumble2/static/css/main.43f11445.chunk.css"
+  },
+  {
+    "revision": "b198500a9cc034375891",
+    "url": "/vumble2/static/js/2.a4eceb99.chunk.js"
+  },
+  {
+    "revision": "eb9fe4f79f693461cdba",
+    "url": "/vumble2/static/js/main.49e6c563.chunk.js"
+  },
+  {
+    "revision": "cf40b6f0dc0fb4b0e763",
+    "url": "/vumble2/static/js/runtime-main.a6e2b043.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vumble2/precache-manifest.08eec2f658e4c5bf2492db0d1c97aab7.js"
+  "/vumble2/precache-manifest.a10b29372585f0630c77d3321c42582f.js"
 );
 
 self.addEventListener('message', (event) => {
