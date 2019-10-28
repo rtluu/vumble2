1b787322e0877504c5bc41320a4e7d810962f17f
Ryan Luu <ryan@vumble.com>
2019-10-27 22:47:53 -0400

    Updates

--- precache-manifest.ad6160fbbf102da45aebc8ebb033bcd8.js
+++ precache-manifest.ad6160fbbf102da45aebc8ebb033bcd8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81ef13c53585db02811a7daa019d91fe",
+    "revision": "9650372f04030d075d0cd64ec3ce0295",
     "url": "/vumble2/index.html"
   },
   {
-    "revision": "25c48ec5ca56e20c7da7",
+    "revision": "951c05e2cb3ee66c3235",
     "url": "/vumble2/static/css/main.79a013b6.chunk.css"
   },
   {
@@ -12,16 +12,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vumble2/static/js/2.f866cd27.chunk.js"
   },
   {
-    "revision": "25c48ec5ca56e20c7da7",
-    "url": "/vumble2/static/js/main.08d45076.chunk.js"
+    "revision": "951c05e2cb3ee66c3235",
+    "url": "/vumble2/static/js/main.78f4b825.chunk.js"
   },
   {
     "revision": "b7edab660bff709a0d09",
     "url": "/vumble2/static/js/runtime-main.676e0dec.js"
   },
   {
+    "revision": "a2d27adbb05c1008f70765bec89e7427",
+    "url": "/vumble2/static/media/comments.a2d27adb.svg"
+  },
+  {
     "revision": "a9b06a06c4011435017a0c33b5681b07",
     "url": "/vumble2/static/media/down-arrow.a9b06a06.svg"
+  },
+  {
+    "revision": "60dffe6b70eebf6e5935773216d67e4c",
+    "url": "/vumble2/static/media/gfycat-logo.60dffe6b.svg"
   },
   {
     "revision": "24d45ed1c779a2d503886efa596affec",
@@ -40,6 +48,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vumble2/static/media/new.7582d8f0.svg"
   },
   {
+    "revision": "3847fc58dd09c56ef44a7c63e1842f76",
+    "url": "/vumble2/static/media/reddit-logo.3847fc58.svg"
+  },
+  {
     "revision": "30f7ea7eaecb7f2dd1e70002e6a23668",
     "url": "/vumble2/static/media/rising.30f7ea7e.svg"
   },
@@ -52,7 +64,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vumble2/static/media/top.31ed0e0c.svg"
   },
   {
+    "revision": "4f6b933c424f43ebcaa5d4dd01e287ed",
+    "url": "/vumble2/static/media/upvote.4f6b933c.svg"
+  },
+  {
     "revision": "5ab8b2abd402d84d7fb6357d18f38ea0",
     "url": "/vumble2/static/media/vumble-logo.5ab8b2ab.svg"
+  },
+  {
+    "revision": "3bef7f9b7a42c2dc05fadb2283348725",
+    "url": "/vumble2/static/media/youtube-logo.3bef7f9b.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vumble2/precache-manifest.a1409cd2fd7c5277020d3e5b5789d143.js"
+  "/vumble2/precache-manifest.ad6160fbbf102da45aebc8ebb033bcd8.js"
 );
 
 self.addEventListener('message', (event) => {
