398356b52ff709733c029f724fbfdb207d3c4351
Ryan Luu <ryan@vumble.com>
2019-11-13 20:54:17 -0500

    Removed unnecessary function

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import styled from "styled-components";
 import { Waypoint } from 'react-waypoint';
 import ReactGA from 'react-ga';
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-152512551-1');
-  ReactGA.pageview('/home');
-}
+ReactGA.initialize('UA-152512551-1');
+ReactGA.pageview('/home');
+
 
 const AppStyled = styled.div`
   
