f632d5f6d7438d30c776d2562b32df48406f278d
Ryan Luu <rtluu@users.noreply.github.com>
2019-12-05 21:56:30 -0500

    Revert "About Page"

--- src/index.js
+++ src/index.js
@@ -2,28 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import About from './About';
 import * as serviceWorker from './serviceWorker';
 
-import { HashRouter, Link, Route } from 'react-router-dom';
-import { createBrowserHistory } from "history"
-
-ReactDOM.render(
-
-    <HashRouter >
-        <div>
-            <Route exact path="/" component={App} />
-            <Route exact path="/about" component={About} />
-        </div>
-    </HashRouter>
-    ,
-    document.getElementById('root')
-);
-
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
